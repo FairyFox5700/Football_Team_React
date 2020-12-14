de43d79ddc1bfa1d68be54b2d359852ef6c98927
Ira Tyschenko <tyschenk40@gmail.com>
2020-12-14 18:47:48 +0200

    Changed

---
+++ gatsby-node.js
@@ -0,0 +1,13 @@
+﻿exports.onCreatePage = async ({ page, actions }) => {
+    const { createPage } = actions;
+
+    // Only update the `/blogs` page.
+    if (page.path.match(/^\/clubs/)) {
+        // page.matchPath is a special key that's used for matching pages
+        // with corresponding routes only on the client.
+        page.matchPath = "/clubs/*";
+
+        // Update the page.
+        createPage(page)
+    }
+}
